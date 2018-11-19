6505d899d68d8bdb30f9b90f7802595379822a90
Christopher Szatmary <cs@christopherszatmary.com>
2018-11-18 21:34:34 -0500

    Added utils to easily execute sql statements on the oracledb

---
+++ src/utils/executeSql.ts
@@ -0,0 +1,50 @@
+import oracledb, { IConnection, IExecuteReturn } from 'oracledb';
+
+import oracleConfig from '../config/oracle';
+
+const doRelease = (connection: IConnection) =>
+  connection.close(err => {
+    if (err) {
+      console.error(err.message);
+    }
+  });
+
+const single = (sql: string): Promise<IExecuteReturn> =>
+  new Promise(async (resolve, reject) => {
+    let connection: IConnection | undefined;
+
+    try {
+      connection = await oracledb.getConnection(oracleConfig);
+      const result = await connection.execute(sql);
+      resolve(result);
+    } catch (err) {
+      reject(err);
+    } finally {
+      if (connection) {
+        doRelease(connection);
+      }
+    }
+  });
+
+const batch = (sqlStatements: string[]): Promise<IExecuteReturn[]> =>
+  new Promise(async (resolve, reject) => {
+    let connection: IConnection | undefined;
+
+    try {
+      connection = await oracledb.getConnection(oracleConfig);
+      const executionPromises = sqlStatements.map(sql =>
+        connection!.execute(sql),
+      );
+      // @ts-ignore
+      const results = await Promise.all(executionPromises);
+      resolve(results);
+    } catch (err) {
+      reject(err);
+    } finally {
+      if (connection) {
+        doRelease(connection);
+      }
+    }
+  });
+
+export default { single, batch };
