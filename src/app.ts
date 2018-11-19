062ee9c0d2f16496fdb4aa57a3a17f25a7f84a14
Christopher Szatmary <cs@christopherszatmary.com>
2018-11-18 21:49:13 -0500

    Added routes to express app

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,36 @@
-import express from 'express';
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
-import helloRoutes from './routes/hello';
+import createRoutes from './routes/create';
+import deleteRoutes from './routes/delete';
+import populateRoutes from './routes/populate';
+import queryRoutes from './routes/query';
+import { handleErrors } from './utils/errorHandlers';
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/', helloRoutes);
+app.use('/api/create', createRoutes);
+app.use('/api/delete', deleteRoutes);
+app.use('/api/populate', populateRoutes);
+app.use('/api/query', queryRoutes);
+
+app.use(handleErrors);
+
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  // Express will server up production assets from the client
+  app.use(express.static(path.resolve(__dirname, 'client')));
+
+  // Express will serve up the index.html file if it doesn't recognize the route
+  app.get('*', (req: Request, res: Response) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
+  });
+}
 
 export default app;
