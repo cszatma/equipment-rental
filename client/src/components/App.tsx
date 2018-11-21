d49ba983877a2b685ab92cf3c5897e12a0bb4043
Christopher Szatmary <christopherszatmary@gmail.com>
2018-11-21 18:44:22 -0500

    Renamed MainMenu to Menu, added Navbar to App

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,26 +1,27 @@
 import React, { FunctionComponent } from 'react';
+import { BrowserRouter, Redirect, Route } from 'react-router-dom';
 import { Container } from 'reactstrap';
 
 import '@styles/App.scss';
-import MainMenu from './MainMenu';
+import Menu from './Menu';
 import Create from './Create';
 import Delete from './Delete';
 import Populate from './Populate';
 import Query from './Query';
-
-import { BrowserRouter, Redirect, Route } from 'react-router-dom';
+import Navbar from '@components/Navbar';
 
 const redirectToMenu = () => <Redirect to="/" />;
 
 const App: FunctionComponent = () => (
   <BrowserRouter>
-    <Container>
-      <Route exact path="/" component={MainMenu} />
+    <Container className="d-flex">
+      <Navbar items={['menu', 'create', 'delete', 'populate', 'query']} />
+      <Route exact path="/" component={Menu} />
       <Route path="/create" component={Create} />
       <Route path="/delete" component={Delete} />
       <Route path="/populate" component={Populate} />
       <Route path="/query" component={Query} />
-      <Route path="/mainmenu" render={redirectToMenu} />
+      <Route path="/menu" render={redirectToMenu} />
     </Container>
   </BrowserRouter>
 );
--- client/src/components/Menu.tsx
+++ client/src/components/Menu.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class MainMenu extends Component<{}> {
+export default class Menu extends Component<{}> {
   public render() {
     return <h1>MainMenu</h1>;
   }
